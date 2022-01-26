6d760de6998b20c92f3f1aae877bc97d6ac2a6bc
qriticual <62610168+qriticual@users.noreply.github.com>
2022-01-26 11:52:16 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,12 +8,11 @@ function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
-
   //Load bodyPix
   const runBodysegment = async () => {
     const net = await bodyPix.load();
     console.log("BodyPix model loaded.");
-    //  Loop and detect hands
+    //Loop and detect hands
     setInterval(() => {
       detect(net);
     }, 100);
@@ -46,27 +45,23 @@ function App() {
       // *   - net.segmentPersonParts
       // *   - net.segmentMultiPerson
       // *   - net.segmentMultiPersonParts
-      // const person = await net.segmentPerson(video);
       const person = await net.segmentPersonParts(video);
       console.log(person);
 
-      // const coloredPartImage = bodyPix.toMask(person);
-      const coloredPartImage = bodyPix.toColoredPartMask(person);
-      const opacity = 0.7;
-      const flipHorizontal = false;
-      const maskBlurAmount = 0;
-      const canvas = canvasRef.current;
+      console.log(person.allPoses[0].keypoints[10].position);
 
-      bodyPix.drawMask(
-        canvas,
-        video,
-        coloredPartImage,
-        opacity,
-        maskBlurAmount,
-        flipHorizontal
-      );
+      draw(person.allPoses[0].keypoints[10].position.x, person.allPoses[0].keypoints[10].position.y);
+
     }
   };
+
+  function draw(x,y) {
+      var canvas = document.getElementById('board');
+
+          var ctx = canvas.getContext('2d');
+
+          ctx.fillRect(x,y, 20, 20);
+  }
 
   runBodysegment()
 
@@ -74,22 +69,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Webcam
-          ref={webcamRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 9,
-            width: 640,
-            height: 480,
-          }}
-        />
 
-        <canvas
+          <Webcam
+              ref={webcamRef}
+              style={{
+                  position: "absolute",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                  left: 0,
+                  right: 0,
+                  textAlign: "center",
+                  zindex: 9,
+                  width: 640,
+                  height: 480,
+              }}
+          />
+
+        <canvas id="board"
           ref={canvasRef}
           style={{
             position: "absolute",
